ebd3a7b1c76d14c9b902dce33c8003eaa0936893
ibsby <16ismith98@gmail.com>
2020-06-28 16:27:48 -0400

    Fixed time zone bug

--- bot.js
+++ bot.js
@@ -22,8 +22,9 @@ function tweetIt() {
 
 //Call tweetIt at 11:11pm
 var now = new Date();
-//Heroku server is two hours fast
-var difference = new Date(now.getFullYear(), now.getMonth() ,now.getDate(), 20, 05, 05, 0) - now;
+//Subtract the current time from the time I want the bot to tweet (11:11pm)
+//The Heroku server is two hours fast so set the time I want
+var difference = new Date(now.getFullYear(), now.getMonth() ,now.getDate(), 01, 11, 05, 0) - now;
 if(difference < 0) {
 	difference += 86400000;	//It's after 11:11pm so do it tommorow
 }
